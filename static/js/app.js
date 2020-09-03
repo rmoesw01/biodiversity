bb5e8e2356e52a82a406f6ab73f6018878600a6d
Becki Oeswein Leeds <rmoesw01@gmail.com>
2020-09-03 19:13:57 -0400

    needle on the gauge

--- static/js/app.js
+++ static/js/app.js
@@ -41,31 +41,73 @@ function optionChanged(value) {
         }); 
         console.log(wash);
 
-        var gaugeTrace = {
-            value: wash,
-            type: 'indicator',
-            mode: 'gauge+number',
-            domain: {
-                x: [0,1],
-                y: [0,1]
-            }
+        // var gaugeTrace = {
+        //     value: wash,
+        //     type: 'indicator',
+        //     mode: 'gauge+number+needle',
+        //     domain: {
+        //         x: [0,1],
+        //         y: [0,1]
+        //     }
             // gauge: {
             //     axis: {range: [null, 9]},
             //     steps: [
             //         range
             //     ]
             // }
-        }
+        // }
+        var traceGauge = {
+            type: 'pie',
+            showlegend: false,
+            hole: 0.4,
+            rotation: 90,
+            values: [ 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
+            text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+            direction: 'clockwise',
+            textinfo: 'text',
+            textposition: 'inside',
+            marker: {
+              colors: ['','','','','','','','','','white'],
+              labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+              hoverinfo: 'label'
+            }
+          }
+      
+          // needle
+          var degrees = (180/9) * wash
+          var radius = 0.6
+          var radians = degrees * Math.PI / 180
+          var x = -1 * radius * Math.cos(radians)
+          var y = radius * Math.sin(radians)
+      
+          var gaugeLayout = {
+            shapes: [{
+              type: 'line',
+              x0: 0.5,
+              y0: 0.5,
+              x1: x + 0.5,
+              y1: y + 0.5,
+              line: {
+                color: 'black',
+                width: 3
+              }
+            }],
+            title: 'Chart',
+            xaxis: {visible: false, range: [-1, 1]},
+            yaxis: {visible: false, range: [-1, 1]}
+          }
+      
+          var dataGauge = [traceGauge]
+        // gaugeData = [gaugeTrace];
 
-        gaugeData = [gaugeTrace];
+        // var gaugeLayout = {
+        //     width: 600, 
+        //     height: 500
+        //     // margin: { t: 0, b: 0 }
+        // }
 
-        var gaugeLayout = {
-            width: 600, 
-            height: 500
-            // margin: { t: 0, b: 0 }
-        }
-
-        Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+        // Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+        Plotly.newPlot('gauge', dataGauge, gaugeLayout);
 
         var trace1 = {
             x: sampleValues.slice(0, 10).reverse(),
